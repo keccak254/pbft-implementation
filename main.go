ad9b1d587a33a4845dc35b4e3f5d8bdda830017f
keccak254 <163550206+keccak254@users.noreply.github.com>
2024-09-23 16:26:09 +0900

    update

--- main.go
+++ main.go
@@ -1,12 +1,39 @@
 package main
 
 import (
-    "os"
+    "flag"
+    "fmt"
+    "log"
+    "net/http"
     "github.com/keccak254/pbft-implementation/network"
 )
 
 func main() {
-    nodeID := os.Args[1]
-    server := network.NewServer(nodeID)
-    server.Start()
+    nodeID := flag.String("id", "", "Node ID")
+    flag.Parse()
+
+    if *nodeID == "" {
+        log.Fatal("Please provide a node ID using the -id flag")
+    }
+
+    node := network.NewNode(*nodeID)
+
+    http.HandleFunc("/req", node.HandleRequest)
+    http.HandleFunc("/preprepare", node.HandlePrePrepare)
+    http.HandleFunc("/prepare", node.HandlePrepare)
+    http.HandleFunc("/commit", node.HandleCommit)
+
+
+    port := "1111"
+    switch *nodeID {
+    case "MS":
+        port = "1112"
+    case "Google":
+        port = "1113"
+    case "IBM":
+        port = "1114"
+    }
+
+    fmt.Printf("Node %s listening on port %s\n", *nodeID, port)
+    log.Fatal(http.ListenAndServe(":"+port, nil))
 }
