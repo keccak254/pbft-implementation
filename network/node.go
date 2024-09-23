1546f51c526ad00b542c74bb030d7ff675c700fa
keccak254 <163550206+keccak254@users.noreply.github.com>
2024-09-23 16:01:26 +0900

    update

--- network/node.go
+++ network/node.go
@@ -198,7 +198,288 @@ func (node *Node) GetCommit(commitMsg *consensus.VoteMsg)error{
 	return nil
 }
 
+func (node *Node) GetCommit(commitMsg *consensus.VoteMsg) error {
+	LogMsg(commitMsg)
 
+	replyMsg, committedMsg, err := node.CurrentState.Commit(commitMsg)
+	if err != nil {
+		return err
+	}
 
+	if replyMsg != nil {
+		if committedMsg == nil {
+			return errors.New("committed message is nil, even though the reply message is not nil")
+		}
 
+		replyMsg.NodeID = node.NodeID
 
+		node.CommittedMsgs = append(node.CommittedMsgs, committedMsg)
+
+		LogStage("Commit", true)
+		node.Reply(replyMsg)
+		LogStage("Reply", true)
+	}
+
+	return nil
+}
+
+func (node *Node) GetReply(msg *consensus.ReplyMsg) {
+	fmt.Printf("Result: %s by %s\n", msg.Result, msg.NodeID)
+}
+
+func (node *Node) createStateForNewConsensus() error {
+	if node.CurrentState != nil {
+		return errors.New("another consensus is ongoing")
+	}
+
+	var lastSequenceID int64
+	if len(node.CommittedMsgs) == 0 {
+		lastSequenceID = -1
+	} else {
+		lastSequenceID = node.CommittedMsgs[len(node.CommittedMsgs) - 1].SequenceID
+	}
+
+	node.CurrentState = consensus.CreateState(node.View.ID, lastSequenceID)
+
+	LogStage("Create the replica status", true)
+
+	return nil
+}
+
+func (node *Node) dispatchMsg() {
+	for {
+		select {
+		case msg := <-node.MsgEntrance:
+			err := node.routeMsg(msg)
+			if err != nil {
+				fmt.Println(err)
+			}
+		case <-node.Alarm:
+			err := node.routeMsgWhenAlarmed()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+}
+
+func (node *Node) routeMsg(msg interface{}) []error {
+	switch msg.(type) {
+	case *consensus.RequestMsg:
+		if node.CurrentState == nil {
+			msgs := make([]*consensus.RequestMsg, len(node.MsgBuffer.ReqMsgs))
+			copy(msgs, node.MsgBuffer.ReqMsgs)
+
+			msgs = append(msgs, msg.(*consensus.RequestMsg))
+
+			node.MsgBuffer.ReqMsgs = make([]*consensus.RequestMsg, 0)
+
+			node.MsgDelivery <- msgs
+		} else {
+			node.MsgBuffer.ReqMsgs = append(node.MsgBuffer.ReqMsgs, msg.(*consensus.RequestMsg))
+		}
+	case *consensus.PrePrepareMsg:
+		if node.CurrentState == nil {
+			msgs := make([]*consensus.PrePrepareMsg, len(node.MsgBuffer.PrePrepareMsgs))
+			copy(msgs, node.MsgBuffer.PrePrepareMsgs)
+
+			msgs = append(msgs, msg.(*consensus.PrePrepareMsg))
+
+			node.MsgBuffer.PrePrepareMsgs = make([]*consensus.PrePrepareMsg, 0)
+
+			node.MsgDelivery <- msgs
+		} else {
+			node.MsgBuffer.PrePrepareMsgs = append(node.MsgBuffer.PrePrepareMsgs, msg.(*consensus.PrePrepareMsg))
+		}
+	case *consensus.VoteMsg:
+		if msg.(*consensus.VoteMsg).MsgType == consensus.PrepareMsg {
+			if node.CurrentState == nil || node.CurrentState.CurrentStage != consensus.PrePrepared {
+				node.MsgBuffer.PrepareMsgs = append(node.MsgBuffer.PrepareMsgs, msg.(*consensus.VoteMsg))
+			} else {
+				msgs := make([]*consensus.VoteMsg, len(node.MsgBuffer.PrepareMsgs))
+				copy(msgs, node.MsgBuffer.PrepareMsgs)
+
+				msgs = append(msgs, msg.(*consensus.VoteMsg))
+
+				node.MsgBuffer.PrepareMsgs = make([]*consensus.VoteMsg, 0)
+
+				node.MsgDelivery <- msgs
+			}
+		} else if msg.(*consensus.VoteMsg).MsgType == consensus.CommitMsg {
+			if node.CurrentState == nil || node.CurrentState.CurrentStage != consensus.Prepared {
+				node.MsgBuffer.CommitMsgs = append(node.MsgBuffer.CommitMsgs, msg.(*consensus.VoteMsg))
+			} else {
+
+				msgs := make([]*consensus.VoteMsg, len(node.MsgBuffer.CommitMsgs))
+				copy(msgs, node.MsgBuffer.CommitMsgs)
+
+				msgs = append(msgs, msg.(*consensus.VoteMsg))
+
+				node.MsgBuffer.CommitMsgs = make([]*consensus.VoteMsg, 0)
+
+				node.MsgDelivery <- msgs
+			}
+		}
+	}
+	return nil
+}
+
+func (node *Node) routeMsgWhenAlarmed() []error {
+	if node.CurrentState == nil {
+
+		if len(node.MsgBuffer.ReqMsgs) != 0 {
+			msgs := make([]*consensus.RequestMsg, len(node.MsgBuffer.ReqMsgs))
+			copy(msgs, node.MsgBuffer.ReqMsgs)
+
+			node.MsgDelivery <- msgs
+		}
+
+		if len(node.MsgBuffer.PrePrepareMsgs) != 0 {
+			msgs := make([]*consensus.PrePrepareMsg, len(node.MsgBuffer.PrePrepareMsgs))
+			copy(msgs, node.MsgBuffer.PrePrepareMsgs)
+
+			node.MsgDelivery <- msgs
+		}
+	} else {
+		switch node.CurrentState.CurrentStage {
+		case consensus.PrePrepared:
+
+			if len(node.MsgBuffer.PrepareMsgs) != 0 {
+				msgs := make([]*consensus.VoteMsg, len(node.MsgBuffer.PrepareMsgs))
+				copy(msgs, node.MsgBuffer.PrepareMsgs)
+
+				node.MsgDelivery <- msgs
+			}
+		case consensus.Prepared:
+
+			if len(node.MsgBuffer.CommitMsgs) != 0 {
+				msgs := make([]*consensus.VoteMsg, len(node.MsgBuffer.CommitMsgs))
+				copy(msgs, node.MsgBuffer.CommitMsgs)
+
+				node.MsgDelivery <- msgs
+			}
+		}
+	}
+	return nil
+}
+
+func (node *Node) resolveMsg() {
+	for {
+
+		msgs := <-node.MsgDelivery
+		switch msgs.(type) {
+		case []*consensus.RequestMsg:
+			errs := node.resolveRequestMsg(msgs.([]*consensus.RequestMsg))
+			if len(errs) != 0 {
+				for _, err := range errs {
+					fmt.Println(err)
+				}
+
+			}
+		case []*consensus.PrePrepareMsg:
+			errs := node.resolvePrePrepareMsg(msgs.([]*consensus.PrePrepareMsg))
+			if len(errs) != 0 {
+				for _, err := range errs {
+					fmt.Println(err)
+				}
+
+			}
+		case []*consensus.VoteMsg:
+			voteMsgs := msgs.([]*consensus.VoteMsg)
+			if len(voteMsgs) == 0 {
+				break
+			}
+
+			if voteMsgs[0].MsgType == consensus.PrepareMsg {
+				errs := node.resolvePrepareMsg(voteMsgs)
+				if len(errs) != 0 {
+					for _, err := range errs {
+						fmt.Println(err)
+					}
+				}
+			} else if voteMsgs[0].MsgType == consensus.CommitMsg {
+				errs := node.resolveCommitMsg(voteMsgs)
+				if len(errs) != 0 {
+					for _, err := range errs {
+						fmt.Println(err)
+					}
+				}
+			}
+		}
+	}
+}
+
+func (node *Node) alarmToDispatcher() {
+	for {
+		time.Sleep(ResolvingTimeDuration)
+		node.Alarm <- true
+	}
+}
+
+func (node *Node) resolveRequestMsg(msgs []*consensus.RequestMsg) []error {
+	errs := make([]error, 0)
+
+	for _, reqMsg := range msgs {
+		err := node.GetReq(reqMsg)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	if len(errs) != 0 {
+		return errs
+	}
+
+	return nil
+}
+
+func (node *Node) resolvePrePrepareMsg(msgs []*consensus.PrePrepareMsg) []error {
+	errs := make([]error, 0)
+
+	for _, prePrepareMsg := range msgs {
+		err := node.GetPrePrepare(prePrepareMsg)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	if len(errs) != 0 {
+		return errs
+	}
+
+	return nil
+}
+
+func (node *Node) resolvePrepareMsg(msgs []*consensus.VoteMsg) []error {
+	errs := make([]error, 0)
+
+	for _, prepareMsg := range msgs {
+		err := node.GetPrepare(prepareMsg)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	if len(errs) != 0 {
+		return errs
+	}
+
+	return nil
+}
+
+func (node *Node) resolveCommitMsg(msgs []*consensus.VoteMsg) []error {
+	errs := make([]error, 0)
+
+	for _, commitMsg := range msgs {
+		err := node.GetCommit(commitMsg)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	if len(errs) != 0 {
+		return errs
+	}
+
+	return nil
+}
